6baed38b96ad1700c6af20cf40b9211eddeff998
Frederik Winkelsdorf <winkelsdorf@Frederiks-MBP.fritz.box>
2015-12-19 02:49:31 +0100

    Demo

---
+++ test/TabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarController.swift
+//  test
+//
+//  Created by Frederik Winkelsdorf on 18.12.15.
+//  Copyright © 2015 ipf electronic. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Fix for iOS 8.0 to 8.2:
+        // tabBar.barTintColor = UIColor.whiteColor() // or a light #f9f9f9
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
